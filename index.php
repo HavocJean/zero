21fb932f6c02a1e34c96608f76b7076d3f16aba2
HavocJean <jean.magic@hotmail.com>
2024-08-11 20:55:10 -0300

    inicio html script e php

--- index.php
+++ index.php
@@ -8,26 +8,36 @@
     <title>cryptobit Patch 0.1</title>
 </head>
 <body>
-    <div class="row menu-background">
-        <div class="col-md-4 logo-div">
-            <img src="https://html.dreamitsolution.net/cryptobit/cryptobit/assets/images/logo.png" alt="" class="logo-menu">
+    <div class="all-content">
+        <div class="row menu-background">
+            <div class="col-md-4 logo-div">
+                <img src="https://html.dreamitsolution.net/cryptobit/cryptobit/assets/images/logo.png" alt="" class="logo-menu">
+            </div>
+            <div class="col-md-5 menu-div">
+                <ul class="menu-list">
+                    <li>Home +</li>
+                    <li>About</li>
+                    <li>Service</li>
+                    <li>Page</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <button type="button" class="btn btn-light btn-sign">Sign In</button>
+            </div>
         </div>
-        <div class="col-md-5 menu-div">
-            <ul class="menu-list">
-                <li>Home +</li>
-                <li>About</li>
-                <li>Service</li>
-                <li>Page</li>
-                <li>Blog</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-        <div class="col-md-3">
-            <button type="button" class="btn btn-light btn-sign">Sign In</button>
+        <div class="row first-page">
+            <div class="col-md-6 first-page-left">
+                <div class="left-effect">
+                    <div class="no-trader">No Trading Comission</div>
+                </div>
+            </div>
+            <div class="col-md-6 first-page-right"></div>
         </div>
     </div>
-    <div class="row first-page">
-
+    <div class="row second-page">
+        conteudo2
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
